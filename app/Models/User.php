a379d0793eaae93226ee7e03769ebaeeb47ed9fc
hermawansafrin <hermawansafrin19@gmail.com>
2025-07-20 12:04:08 +0800

    feat: initial fresh installation for default datas

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,13 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasRoles, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -44,5 +42,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @return string
+     */
+    protected function serializeDate(\DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
     }
 }
---
+++ config/values.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'position_span' => 10,
+    'date_format_with_hour' => 'd-m-Y H:i:s',
+    'date_format' => 'd M Y',
+];
